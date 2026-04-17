0e5664c0e5f54cba9b192e101cedf074e0e9ddc8
Marcos <Reimarcosneto3@gmail.com>
2026-04-16 21:57:35 -0300

    Atualiza cadastro de alunos

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 df = open("C:/Users/reima/OneDrive/Documentos/programação/aula de programação/v2.py/basededados.csv", "r")
-conteudo = df.read()
-print(conteudo)
+
 
 while True:
     # -=-=-=-=-=- tela inicial =-=-=-=-=
@@ -16,9 +15,7 @@ while True:
     if esco1 == 1:
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
         print("\n\nCadastro\n")
-        nome = input(
-            "Nome: "
-        )  # Nota: se o nome do aluno for igual a outro ele sobreescreve se alguem puder ajeitar agradeço
+        nome = input("Nome: ")  # Nota: se o nome do aluno for igual a outro ele sobreescreve se alguem puder ajeitar agradeço
         matricula = input("matricula:")
         sexo = input("Sexo(1-fem/2-mas):")
         idade = input("idade:")
@@ -26,5 +23,6 @@ while True:
         curso = input("Curso(1-PHP/2-Java/3-Delphi):")
         with open("basededados.csv", "a") as f:
             f.write(f"{nome},{matricula},{'feminino' if sexo=='1' else 'masculino'},{idade},{'Manhã' if turno=='1' else 'noite'},{'PHP' if curso=='1' else 'Java' if curso=='2' else 'Delphi'}\n")
-        print(df)
+        
+        print((open("C:/Users/reima/OneDrive/Documentos/programação/aula de programação/v2.py/basededados.csv", "r")))
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
