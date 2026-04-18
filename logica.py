1eb424d0965b60bc562c8c67bc1489926a2e60ed
thiagolayme1966 <thiagolayme1966@gmail.com>
2026-04-17 23:03:08 -0300

    thiago

---
+++ logica.py
@@ -0,0 +1,18 @@
+import json as js
+
+# =-=-=-=-=-=-= cria (se n tiver) e abre em dicionario ou abre em formato de dicionario =-=-=-=-=-=- 
+# Nota: esse não pode ser def por outros termos dependerem dele (no caso, dependem do dicionario que será trabalhado durante todo o processo)
+arquivo = open("dados.json", "a")
+arquivo.close()
+arquivo = open("dados.json", "r")
+arquivo1 = arquivo.read()
+arquivo.close()
+if arquivo1 == "":
+    cadastros_pa = { 
+        "aluno_teste" : {"matricula": "matricula teste","sexo": "sexo teste","idade": "idade teste","turno": "turno teste","curso": "PHP",},
+        "aluno_teste2" : {"matricula": "matricula teste2","sexo": "sexo teste2","idade": "idade teste2","turno": "turno teste","curso": "java",},
+        "aluno_teste3" : {"matricula": "matricula teste2","sexo": "sexo teste2","idade": "idade teste2","turno": "turno teste","curso": "PHP",}
+    }
+else:
+    cadastros_pa = js.loads(arquivo1)
+    
