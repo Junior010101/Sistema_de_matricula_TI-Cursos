d323c06170b97089db99d4d9bb33b1a04641d7ac
Marcos <Reimarcosneto3@gmail.com>
2026-04-17 21:00:12 -0300

    oi

--- percistencia.py
+++ percistencia.py
@@ -8,19 +8,17 @@ arquivo = open("dados.json", "r")
 arquivo1 = arquivo.read()
 arquivo.close()
 if arquivo1 == "":
-    cadastros = {
-        "Nome": "teste nome",
-        "matricula": "teste matricula",
-        "sexo": "sexo teste",
-        "idade":"idade texte",
-        "turno":"turno texte",
-        "curso":"curso texte",
+    cadastros_pa = { 
+        "aluno_teste" : {"matricula": "matricula teste","sexo": "sexo teste","idade": "idade teste","turno": "turno teste","curso": "PHP",},
+        "aluno_teste2" : {"matricula": "matricula teste2","sexo": "sexo teste2","idade": "idade teste2","turno": "turno teste","curso": "java",},
+        "aluno_teste3" : {"matricula": "matricula teste2","sexo": "sexo teste2","idade": "idade teste2","turno": "turno teste","curso": "PHP",}
     }
 else:
-    cadastros = js.loads(arquivo1)
+    cadastros_pa = js.loads(arquivo1)
 
-def salvar(): #=-=-=-=-=-=-=-=pega o dicionario de antes e trasforma em str e salva no arquivo Json =-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    arquivo = open("dados.json", "a")
-    js.dump(cadastros, arquivo, indent=4)
+
+def salvar(): #=-=-=-=-=-=-=-=pega o msm dicionario de antes e trasforma em str e salva no arquivo Json =-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    arquivo = open("dados.json", "w")
+    js.dump(cadastros_pa, arquivo, indent=4)
     arquivo.close()
-    print(cadastros)
+salvar()
