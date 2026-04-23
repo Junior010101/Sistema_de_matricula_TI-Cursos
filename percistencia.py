db22a7029527e1e23693e45bb5b1be595171253e
junior010101 <marcondesjunior2209@gmail.com>
2026-04-23 15:41:57 -0300

    Enviando o procedimento salvar_arquivo()

--- percistencia.py
+++ percistencia.py
@@ -1,63 +1,43 @@
-import json as js
+from json import dump
 from pathlib import Path
 
-CAMINHO_JSON = Path.cwd() / "data" / "alunos.json"
+CAMINHO_JSON = Path.cwd() / "dados" / "clientes.json"
 
-# criar (se n tiver) e abre em dicionario ou abre em formato de dicionario
-# Nota: esse não pode ser def por outros termos dependerem dele (no caso,
-# dependem do dicionario que será trabalhado durante todo o processo)
-
-arquivo = open(CAMINHO_JSON, "a")
-arquivo.close()
-arquivo = open(CAMINHO_JSON, "r")
-arquivo1 = arquivo.read()
-arquivo.close()
-
-if arquivo1 == "":
-    cadastros_pa = {
-        "aluno_teste": {
-            "matricula": "matricula teste",
-            "sexo": "sexo teste",
-            "idade": "idade teste",
-            "turno": "turno teste",
-            "curso": "PHP",
-        },
-        "aluno_teste2": {
-            "matricula": "matricula teste2",
-            "sexo": "sexo teste2",
-            "idade": "idade teste2",
-            "turno": "turno teste",
-            "curso": "java",
-        },
-        "aluno_teste3": {
-            "matricula": "matricula teste2",
-            "sexo": "sexo teste2",
-            "idade": "idade teste2",
-            "turno": "turno teste",
-            "curso": "PHP",
-        },
-    }
-else:
-    cadastros_pa = js.loads(arquivo1)
+# Estrutura padronizada do Json:
+# {
+#   "clientes": [
+#     {
+#       "titular": true,
+#       "cpf": "12345678900",
+#       "nome": "João da Silva",
+#       "sexo": "masc",
+#       "email": "joao.silva@example.com",
+#       "data_nascimento": "20-05-1995",
+#       "telefone": "(81) 98888-7777",
+#       "terceiros": [
+#         {
+#           "cpf": "987.654.321-99",
+#           "nome": "Ana Silva",
+#           "data_nascimento": "15-03-2010"
+#         }
+#       ],
+#       "plano_saude": {
+#         "valor": 350.75,
+#         "data_vencimento": "10-05-2026"
+#       }
+#     }
+#   ]
+# }
 
 
-# pega o msm dicionario de antes e trasforma em str e salva no arquivo Json
-def salvar():
-    arquivo = open("dados.json", "w")
-    js.dump(cadastros_pa, arquivo, indent=4)
-    arquivo.close()
+def salvar_arquivo(novos_dados):
+    pasta = CAMINHO_JSON.parent
+    pasta.mkdir(parents=True, exist_ok=True)
 
-
-def ler_arquivo():
-    if not CAMINHO_JSON.exists():
-        print("O arquivo alunos.json não foi encontrado.")
-        return {}
-
-    with open(CAMINHO_JSON, "r") as arquivo:
-        dados = js.load(arquivo)
-        return dados
-
-
-def editar_arquivo(dados):
     with open(CAMINHO_JSON, "w") as arquivo:
-        js.dump(dados, arquivo, indent=4)
+        dump(novos_dados, arquivo, indent=4)
+
+
+# Para @Reimarcosneto3 fazer:
+def ler_arquivo():
+    pass
