e36d5a4055390c050592fc1c7f1fa5db4f5312e3
Marcos <Reimarcosneto3@gmail.com>
2026-04-23 19:39:04 -0300

    atualizado

--- percistencia.py
+++ percistencia.py
@@ -1,37 +1,30 @@
-from json import dump
+from json import dump,loads
 from pathlib import Path
 
 CAMINHO_JSON = Path.cwd() / "dados" / "clientes.json"
 
-# Estrutura padronizada do Json:
-# {
-#     "clientes": [
-#         {
-#             "titular": true,
-#             "cpf": "12345678900",
-#             "nome": "João da Silva",
-#             "sexo": "masc",
-#             "email": "joao.silva@example.com",
-#             "data_nascimento": "20-05-1995",
-#             "telefone": "(81) 98888-7777",
-#             "terceiros": [
-#                 "98765432199"
-#             ],
-#             "plano_saude": {
-#                 "tipo": "Diamante",
-#                 "valor": 350.75,
-#                 "data_vencimento": "10-05-2026"
-#             }
-#         },
-#         {
-#             "titular": false,
-#             "cpf": "98765432199",
-#             "nome": "Ana Silva",
-#             "sexo": "fem",
-#             "data_nascimento": "15-03-2010"
-#         }
-#     ]
-# }
+
+
+clientes = {
+    "titular": True,
+    "cpf": "12345678900",
+    "nome": "João da Silva",
+    "sexo": "masc",
+    "email": "joao.silva@example.com",
+    "data_nascimento": "20-05-1995",
+    "telefone": "(81) 98888-7777",
+    "terceiros": [
+      {
+        "cpf": "987.654.321-99",
+        "nome": "Ana Silva",
+        "data_nascimento": "15-03-2010"
+      }
+    ],
+    "plano_saude": {
+      "valor": 350.75,
+      "data_vencimento": "10-05-2026"
+    }
+  }
 
 
 def salvar_arquivo(novos_dados):
@@ -41,7 +34,14 @@ def salvar_arquivo(novos_dados):
     with open(CAMINHO_JSON, "w") as arquivo:
         dump(novos_dados, arquivo, indent=4)
 
-
+salvar_arquivo(clientes)
 # Para @Reimarcosneto3 fazer:
 def ler_arquivo():
-    pass
+    if CAMINHO_JSON.exists():
+        arquivo = open(CAMINHO_JSON, "r")
+        arquivo1 = arquivo.read()
+        dicionario = loads(arquivo1) #pra facilitar a manipulação de dados dps
+        print(arquivo1)
+    else:
+        print("Arquivo NÃO existe")
+ler_arquivo()
