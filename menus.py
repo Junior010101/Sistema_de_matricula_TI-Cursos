1b4cd12565c027b8384fe3ba9971516c18a391ec
thiagolayme1966 <thiagolayme1966@gmail.com>
2026-04-23 22:35:35 -0300

    thiago

--- menus.py
+++ menus.py
@@ -90,147 +90,38 @@ def cadastrar_cliente():
         case _:
             print("Opção invalida.\n")
 
-
-cadastrar_cliente()
-# def listar_por_curso():
-#     alunos = ler_arquivo()
-
-#     print("\nFiltrar por curso:")
-#     print("1 - PHP")
-#     print("2 - Java")
-#     print("3 - Delphi")
-
-#     curso = int(input("Escolha: "))
-
-#     cursos = {1: "PHP", 2: "Java", 3: "Delphi"}
-#     curso = cursos[curso]
-
-#     print("\n=== LISTAGEM POR CURSO ===")
-
-#     for matricula, aluno in alunos.items():
-#         if curso in aluno["cursos"]:
-#             cursos_nome = [c for c in aluno["cursos"]]
-#             mensalidade = float(aluno["mensalidade"])
-
-#             print(
-#                 f"""
-# Matricula: {matricula}
-# Nome: {aluno['nome']}
-# Sexo: {"F" if aluno['sexo'] == "1" else "M"}
-# Idade: {aluno['idade']}
-# Cursos: {' / '.join(cursos_nome)}
-# Mensalidade: R$ {mensalidade:.2f}
-# --------------------------
-# """
-#             )
-#     input("Pressione ENTER para voltar...")
-
-
-# def listar_por_sexo():
-#     alunos = ler_arquivo()
-
-#     print("\nFiltrar por sexo:")
-#     print("1 - Feminino")
-#     print("2 - Masculino")
-
-#     sexo = int(input("Escolha: "))
-
-#     sexo_list = {1: "F", 2: "M"}
-#     sexo = sexo_list[sexo]
-
-#     print("\n=== LISTAGEM POR SEXO ===")
-
-#     for matricula, aluno in alunos.items():
-#         if aluno["sexo"] == sexo:
-#             cursos_nome = [c for c in aluno["cursos"]]
-#             mensalidade = float(aluno["mensalidade"])
-
-#             print(
-#                 f"""
-# Matricula: {matricula}
-# Nome: {aluno['nome']}
-# Sexo: {"F" if aluno['sexo'] == "1" else "M"}
-# Idade: {aluno['idade']}
-# Cursos: {' / '.join(cursos_nome)}
-# Mensalidade: R$ {mensalidade:.2f}
-# --------------------------
-# """
-#             )
-#     input("Pressione ENTER para voltar...")
-
-
-# def editar_aluno():
-#     alunos = ler_arquivo()
-#     matricula = input("\nDigite a matricula do aluno a ser editado: ")
-
-#     if matricula in alunos:
-#         aluno = alunos[matricula]
-
-#         nome = input(f"Nome atual: {aluno['nome']}. Digite o novo ou (N): ")
-#         if nome.upper() != "N":
-#             aluno["nome"] = nome
-
-#         sexo = input(f"Sexo atual: {aluno['sexo']}. Digite o novo ou (N): ")
-#         if sexo.upper() != "N":
-#             aluno["sexo"] = sexo
-
-#         idade = input(f"Idade atual: {aluno['idade']}. Digite a nova ou (N): ")
-#         if idade.upper() != "N":
-#             aluno["idade"] = int(idade)
-
-#         turno_atual = aluno["turno"]
-#         contraturno = "Manhã" if turno_atual == "Tarde" else "Tarde"
-
-#         mudar_turno = input(
-#             f"Turno atual: {turno_atual}. Mudar para {contraturno}? (S/N): "
-#         )
-#         if mudar_turno.upper() == "S":
-#             aluno["turno"] = contraturno
-
-#         while True:
-#             print(f"Cursos atuais: {aluno['cursos']}")
-#             remover = input("Digite o curso para remover ou (N) para sair: ")
-#             if remover.upper() == "N":
-#                 break
-#             if remover in aluno["cursos"]:
-#                 aluno["cursos"].remove(remover)
-#             else:
-#                 print("Curso não encontrado.")
-
-#         opcoes = {"1": "PHP", "2": "Java", "3": "Delphi"}
-#         while True:
-#             print("\nCursos disponíveis: (1-PHP / 2-Java / 3-Delphi)")
-#             escolha = input(
-#                 "Digite o número do curso"
-#                 + " que deseja adicionar ou (N) para nenhum: "
-#             )
-
-#             if escolha.upper() == "N":
-#                 break
-
-#             if escolha in opcoes:
-#                 curso_nome = opcoes[escolha]
-
-#                 if curso_nome not in aluno["cursos"]:
-#                     aluno["cursos"].append(curso_nome)
-#                     print(f"Curso {curso_nome} adicionado!")
-#                 else:
-#                     print(f"O aluno já está matriculado em {curso_nome}.")
-#             else:
-#                 print("Opção inválida!")
-
-#             continuar = input("\nDeseja cadastrar um outro curso (S ou N): ")
-#             if continuar.upper() != "S":
-#                 break
-
-#         editar_arquivo(alunos)
-#         print("Alterações realizadas com sucesso!")
-#     else:
-#         print(
-#             f"O aluno cuja a matricula é {matricula},"
-#             + " não está cadastrado no sistema."
-#         )
-
-
-# def remover_aluno():
-#     pass
+#thiago
+def editar_cliente():
+    editar = ler_arquivo()
+    cpf = input("Informe o CPF que deseja alterar: ")
+    if cpf in editar:
+        print(
+            "1 - Nome" 
+            + "\n2 - Sexo" 
+            + "\n3 - E-mail" 
+            + "\n4 - Data de Nascimento" 
+            + "\n5 - Telefone"  
+            + "\n6 - Nome de terceiros" 
+            + "\n7 - Data de nascimento de terceiros"
+            )
+        quero_editar = input("Informe o número do dado que você quer editar: ")
+        match quero_editar:
+            case "1":
+                editar[cpf]["nome"] = input("Informe o novo nome: ")
+            case "2":
+                editar[cpf]["sexo"] = input("Informe o novo sexo: ")
+            case "3":
+                editar[cpf]["email"] = input("Informe o novo E-mail: ")
+            case "4":
+                editar[cpf]["data_nascimento"] = input("Informe a nova data de nascimento: ")
+            case "5":
+                editar[cpf]["telefone"] = input("Informe o novo telefone: ")
+            case "6":
+                quero_editar2= input("Informe o CPF do dependente que você quer editar: ")
+                editar[cpf]["terceiros"][quero_editar2]["nome"] = input("Informe o novo nome do dependente: ")
+            case "7":
+                quero_editar2= input("Informe o CPF do dependente que você quer editar: ")
+                editar[cpf]["terceiros"][quero_editar2]["data_nascimento"] = input("Informe a nova data de nascimento: ")
+        return salvar_arquivo(editar)        
+    else:
+        print("Desculpe, o CPF informado não foi encontrado!")
