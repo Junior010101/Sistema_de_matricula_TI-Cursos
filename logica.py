08a50d71b9b0ac01be0aa8c1479641e837f31e67
tgs4ntos <tiagoss255@gmail.com>
2026-04-25 00:21:50 -0300

    atualização2

--- logica.py
+++ logica.py
@@ -57,3 +57,47 @@ def validar_data_nascimento(data_nascimento):
         idade -= 1
 
     return data_nascimento, idade
+
+def calculo(dados):
+    
+    for chave, item in dados.items():
+        idade = dados[chave]["data_nascimento"].split("-")
+        if dados[chave]["plano_saude"]["plano"] == "Diamante":
+            if dados[chave]["sexo"] == "fem" and (int(idade[2]) > (2026 - int(idade[2])) >= 13) and ((2026 - int(idade[2])) <= 35):
+                dados[chave]["plano_saude"]["valor"] = 400 + (400 * 0.3) 
+            if len(dados[chave]["terceiros"]) > 1:
+                dados[chave]["plano_saude"]["valor"] = 400 - (400 * 0.2) 
+            if (2026 - int(idade[2])) < 13:
+                dados[chave]["plano_saude"]["valor"] = 400 - (400 * 0.3) 
+            if (2026 - int(idade[2])) >= 60:
+                dados[chave]["plano_saude"]["valor"] = 400 - (400 * 0.4) 
+                
+        elif dados[chave]["plano_saude"]["plano"] == "Ouro":
+            if dados[chave]["sexo"] == "fem" and (int(idade[2]) > (2026 - int(idade[2])) >= 13) and ((2026 - int(idade[2])) <= 35):
+                dados[chave]["plano_saude"]["valor"] = 300 + (300 * 0.3) 
+            if len(dados[chave]["terceiros"]) > 1:
+                dados[chave]["plano_saude"]["valor"] = 300 - (300 * 0.2) 
+            if (2026 - int(idade[2])) < 13:
+                dados[chave]["plano_saude"]["valor"] = 300 - (300 * 0.3) 
+            if (2026 - int(idade[2])) >= 60:
+                dados[chave]["plano_saude"]["valor"] = 300 - (300 * 0.4) 
+                
+        elif dados[chave]["plano_saude"]["plano"] == "Prata":
+            if dados[chave]["sexo"] == "fem" and (int(idade[2]) > (2026 - int(idade[2])) >= 13) and ((2026 - int(idade[2])) <= 35):
+                dados[chave]["plano_saude"]["valor"] = 200 + (200 * 0.3) 
+            if len(dados[chave]["terceiros"]) > 1:
+                dados[chave]["plano_saude"]["valor"] = 200 - (200 * 0.2) 
+            if (2026 - int(idade[2])) < 13:
+                dados[chave]["plano_saude"]["valor"] = 200 - (200 * 0.3) 
+            if (2026 - int(idade[2])) >= 60:
+                dados[chave]["plano_saude"]["valor"] = 200 - (200 * 0.4) 
+                
+        elif dados[chave]["plano_saude"]["plano"] == "Esmeralda":
+            if dados[chave]["sexo"] == "fem" and (int(idade[2]) > (2026 - int(idade[2])) >= 13) and ((2026 - int(idade[2])) <= 35):
+                dados[chave]["plano_saude"]["valor"] = 500 + (500 * 0.3) 
+            if len(dados[chave]["terceiros"]) > 1:
+                dados[chave]["plano_saude"]["valor"] = 500 - (500 * 0.2) 
+            if (2026 - int(idade[2])) < 13:
+                dados[chave]["plano_saude"]["valor"] = 500 - (500 * 0.3) 
+            if (2026 - int(idade[2])) >= 60:
+                dados[chave]["plano_saude"]["valor"] = 500 - (500 * 0.4) 
